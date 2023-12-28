c2ce0cbe162214edac66c7116dfaf00890e2583f
Amirreza <a.abasian1382@gmail.com>
2023-12-28 12:33:17 +0330

    Migrate to Next.js(14.0.2) via NextUI

---
+++ Front-end/nahak/app/layout.tsx
@@ -0,0 +1,20 @@
+import "@/styles/globals.css";
+import { Providers } from "./providers";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang='en'
+      className='light'
+    >
+      <head />
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
