2c3270c84e1769ef6942fd5704ac73451149d00b
Amirreza <a.abasian1382@gmail.com>
2024-01-10 22:11:54 +0330

    Added functionality for accepting the _string_ from the user

--- Front-end/my-app/app/Paziresh/result/page.tsx
+++ Front-end/my-app/app/Paziresh/result/page.tsx
@@ -3,15 +3,27 @@ import { useMachineInfoStore, useTransitionStore } from "@/app/store";
 import MachineDrawer from "@/components/MachineDrawer";
 import MainFrame from "@/components/MainFrame";
 import { Chip } from "@nextui-org/react";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Result = () => {
+  const [inputString, setInputString] = useState("");
+
+  const { push } = useRouter();
+
   const states = useMachineInfoStore((state) => state._states);
   const alphabets = useMachineInfoStore((state) => state.alphabets);
   const initialState = useMachineInfoStore((state) => state.initialState);
   const finalStates = useMachineInfoStore((state) => state.finalStates);
   const transitions = useTransitionStore((state) => state.transitions);
+  const resetTransitions = useTransitionStore((state) => state.reset);
+  const resetMachine = useMachineInfoStore((state) => state.reset);
 
+  // here we handle the string confirmation from tha API
+  function handleUserString() {
+    // a request to API with answer will come here
+    setInputString("");
+  }
   console.log(
     "Transitions : ",
     transitions,
@@ -25,8 +37,8 @@ const Result = () => {
     "\nfinalStates :",
     finalStates
   );
-  //TODO make an optimized transitions array so if the 'from' and 'to' keys are the same it will make it one object : {from:'A' , to:'A' , label:'a,b'}
-  // or I can make this changes in the store
+
+  // Optimize states (key names are changed to be compatible with VIS.js)
   const optimizedState = states.map((state) => ({ id: state, label: state }));
 
   return (
@@ -53,6 +65,35 @@ const Result = () => {
         initialState={initialState}
         finalStates={finalStates}
       />
+      <div className='mt-6 flex'>
+        <button
+          onClick={handleUserString}
+          className='basis-1/4 bg-darkColor font-semibold md:text-lg rounded-l-lg text-primaryColor hover:bg-darkMediumColor transition-all active:scale-95'
+        >
+          بررسی
+        </button>
+        <input
+          onChange={(e) => setInputString(e.target.value)}
+          placeholder='در این قسمت رشته را وارد کنید'
+          dir='rtl'
+          className='basis-3/4 bg-[#D9D9D9] p-4 placeholder:text-darkColor rounded-r-xl outline-none'
+          type='text'
+        />
+      </div>
+
+      {/* Here the user strings come and is waiting for its confirmation ↓ */}
+      <section className='my-4'>123</section>
+      <div className='flex'>
+        <button
+          onClick={() => (
+            resetTransitions(), resetMachine(), push("/Paziresh")
+          )}
+          className='basis-1/4 bg-darkColor p-4 font-semibold md:text-lg rounded-lg text-primaryColor hover:bg-darkMediumColor transition-all active:scale-95'
+        >
+          طراحی ماشین جدید
+        </button>
+        <div className='basis-3/4' />
+      </div>
     </MainFrame>
   );
 };
