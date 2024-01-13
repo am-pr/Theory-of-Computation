5db2bead0989055d30c799eba1d881ef29207828
Amirreza <a.abasian1382@gmail.com>
2024-01-13 22:18:04 +0330

    Added Compliment page

--- Front-end/my-app/app/Compliment/page.tsx
+++ Front-end/my-app/app/Compliment/page.tsx
@@ -1,8 +1,28 @@
+"use client";
+import MachineInfo from "@/components/MachineInfo";
 import MainFrame from "@/components/MainFrame";
-import React from "react";
-
+import { Chip } from "@nextui-org/react";
+import { useMachineInfoStore } from "../store";
+import { useTransitionStore } from "../store";
 const Compliment = () => {
-  return <MainFrame>Compliment</MainFrame>;
+  const resetTransitions = useTransitionStore((state) => state.reset);
+  const resetMachine = useMachineInfoStore((state) => state.reset);
+
+  resetMachine();
+  resetTransitions();
+
+  return (
+    <MainFrame>
+      <header className='flex justify-between'>
+        <div />
+        <h1 className='text-2xl font-black text-darkColor'>متمم گرفتن</h1>
+      </header>
+      <span className='block font-medium text-right mt-9 mb-7 text-darkColor'>
+        ماشین را رسم کنید
+      </span>
+      <MachineInfo redirectPathname='/Compliment/result' />
+    </MainFrame>
+  );
 };
 
 export default Compliment;
