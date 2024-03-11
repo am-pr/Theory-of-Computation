bc7dd5089e22c34767620d6c2c76f3e6872bc3f1
Amirreza <a.abasian1382@gmail.com>
2024-03-11 21:59:44 +0330

    Added framer-motion effects to the landing page

--- Front-end/my-app/app/page.tsx
+++ Front-end/my-app/app/page.tsx
@@ -6,7 +6,23 @@ import background from "@/public/homepage/background/background.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
+//motion variants :
+const imgVariants = {
+  hidden: {
+    opacity: 0,
+    color: "rgba(0,0,0,0)",
+  },
+  visible: {
+    opacity: 1,
+    color: "#f4ce14",
+    transition: {
+      delay: 1,
+      duration: 1,
+    },
+  },
+};
 
 export default function Home() {
   return (
@@ -19,18 +35,21 @@ export default function Home() {
         <p className='absolute z-30 text-center text-primaryColor text-3xl font-bold w-full top-[21%]'>
           ساخت راحت ، تبدیل سریع
         </p>
-        
+
         <TypeAnimation
           className='absolute z-30 text-center text-5xl font-bold w-full bottom-[45%]'
           sequence={[1000, "همین حالا ماشین خودتو بساز"]}
           wrapper='span'
           cursor={true}
           speed={22}
-          style={{ fontSize: "3rem", display: "inline-block", color: "#F4CE14" }}
+          style={{
+            fontSize: "3rem",
+            display: "inline-block",
+            color: "#F4CE14",
+          }}
         />
 
         <div className='w-full items-center flex justify-center'>
-
           <Link
             href={"/AutomataMaker"}
             className='btnfos btnfos-1 absolute z-30 top-[60%] right-1/2 translate-x-1/2 bg-primaryColor/70 text-center rounded-xl overflow-hidden'
@@ -46,10 +65,10 @@ export default function Home() {
           className='absolute z-30 text-primaryColor text-3xl font-bold w-full bottom-[7%] pl-10 '
           href='#'
         >
-          این سایت چیکار می کنه؟{" "}
+          این سایت چیکار می کنه؟
         </a>
 
-        {/* <background> */}
+        {/* #region background filters */}
         <div className='w-full h-screen bg-[#45474B]/25 z-20 absolute' />
         <div className='w-full h-screen bg-black/65 z-10 absolute' />
         <Image
@@ -58,57 +77,189 @@ export default function Home() {
           className='w-full h-screen z-0'
           quality={100}
         />
-        {/* </background> */}
+        {/* #endregion */}
       </main>
-      <main className="bg-[#57595D] h-screen">
-        <p className="text-primaryColor text-5xl w-full text-center p-14 font-bold">عملگـــرها</p>
-        <div className="flex justify-center  ">
-          <div className="grid grid-cols-3 gap-48">
-            <div className="flex justify-center items-center ">
-              <Link href={'/Paziresh'}>
-                <img src='/homepage/icons/stringAccepterIcon.svg' alt="stringAccepter icon" height='180px' width='180px'/>
-                <p className="text-white w-[180px] flex justify-center absolute mt-6 font-bold">پذیرش رشته</p>
+      <main className='bg-darkMediumColor h-screen'>
+        <motion.p
+          className='text-primaryColor text-5xl w-full text-center p-14 pt-20 font-bold'
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 200 }}
+          whileInView={{
+            opacity: 1,
+            y: "0",
+          }}
+        >
+          عملگـــرها
+        </motion.p>
+        <div className='flex justify-center items-center'>
+          <div className='grid grid-cols-3 gap-48'>
+            <div>
+              <Link
+                className='flex flex-col justify-center items-center'
+                href={"/Paziresh"}
+              >
+                <motion.img
+                  variants={imgVariants}
+                  whileInView='visible'
+                  initial='hidden'
+                  src='/homepage/icons/stringAccepterIcon.svg'
+                  alt='stringAccepter icon'
+                  height='180px'
+                  width='180px'
+                />
+                <motion.p
+                  initial={{ opacity: 0, y: -100 }}
+                  transition={{ duration: 1, type: "spring", stiffness: 50 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  className='text-white w-[180px] text-center mt-4 font-bold'
+                >
+                  پذیرش رشته
+                </motion.p>
               </Link>
             </div>
-            <div className="flex justify-center">
-              <Link href={'/Minimize'}>
-                <img src='/homepage/icons/minimizeIcon.svg' alt="minimize icon" height='160px' width='160px'/>
-                <p className="text-white w-[180px] flex justify-center absolute mt-6 font-bold">مینیمایز کردن</p>
+            <div>
+              <Link
+                className='flex flex-col justify-center items-center'
+                href={"/Minimize"}
+              >
+                <motion.img
+                  variants={imgVariants}
+                  whileInView='visible'
+                  initial='hidden'
+                  src='/homepage/icons/minimizeIcon.svg'
+                  alt='minimize icon'
+                  height='160px'
+                  width='160px'
+                />
+                <motion.p
+                  initial={{ opacity: 0, y: -100 }}
+                  transition={{ duration: 1, type: "spring", stiffness: 50 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  className='text-white w-[180px] text-center mt-6 font-bold'
+                >
+                  مینیمایز کردن
+                </motion.p>
               </Link>
             </div>
-            <div className="flex justify-center">
-              <Link href={'/Compliment'}>
-                <img src='/homepage/icons/complimentIcon.svg' alt="compliment icon" height='150px' width='150px'/>
-                <p className="text-white w-[180px] flex justify-center absolute mt-6 font-bold">متمم گرفتن</p>
-
+            <div>
+              <Link
+                className='flex flex-col justify-center items-center'
+                href={"/Compliment"}
+              >
+                <motion.img
+                  variants={imgVariants}
+                  whileInView='visible'
+                  initial='hidden'
+                  src='/homepage/icons/complimentIcon.svg'
+                  alt='compliment icon'
+                  height='150px'
+                  width='150px'
+                />
+                <motion.p
+                  className='text-white w-[180px] text-center mt-6 font-bold'
+                  initial={{ opacity: 0, y: -100 }}
+                  transition={{ duration: 1, type: "spring", stiffness: 50 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                >
+                  متمم گرفتن
+                </motion.p>
               </Link>
             </div>
           </div>
         </div>
 
-
-        <p className="text-primaryColor text-5xl w-full text-center p-16 mt-14 font-bold">ابـــــــــزار ها</p>
-        <div className="flex justify-center  ">
-          <div className="grid grid-cols-3 gap-48 ">
-            <div className="flex justify-center ">
-              <Link href={'/Ejtema'}>
-                <img src='/homepage/icons/unionIcon.svg' alt="union icon" height='180px' width='180px'/>
-                <p className="text-white w-[180px] flex justify-center absolute mt-6 font-bold">اجتماع</p>
-
+        <motion.p
+          className='text-primaryColor text-5xl w-full text-center p-14 font-bold'
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 200 }}
+          whileInView={{
+            opacity: 1,
+            y: "0",
+          }}
+        >
+          ابـــــــــزار ها
+        </motion.p>
+        <div className='flex justify-center  '>
+          <div className='grid grid-cols-3 gap-48 '>
+            <div className='flex justify-center '>
+              <Link href={"/Ejtema"}>
+                <motion.img
+                  variants={imgVariants}
+                  whileInView='visible'
+                  initial='hidden'
+                  src='/homepage/icons/unionIcon.svg'
+                  alt='union icon'
+                  height='180px'
+                  width='180px'
+                />
+                <motion.p
+                  initial={{ opacity: 0, y: -100 }}
+                  transition={{ duration: 1, type: "spring", stiffness: 50 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  className='text-white w-[180px] text-center mt-6 font-bold'
+                >
+                  اجتماع
+                </motion.p>
               </Link>
             </div>
-            <div className="flex justify-center">
-              <Link href={'/Eshterak'}>
-                <img src='/homepage/icons/intersectionIcon.svg' alt="intersection icon" height='160px' width='160px'/>
-                <p className="text-white w-[180px] flex justify-center absolute mt-6 font-bold">اشتراک</p>
-
+            <div className='flex justify-center'>
+              <Link href={"/Eshterak"}>
+                <motion.img
+                  variants={imgVariants}
+                  whileInView='visible'
+                  initial='hidden'
+                  src='/homepage/icons/intersectionIcon.svg'
+                  alt='intersection icon'
+                  height='160px'
+                  width='160px'
+                />
+                <motion.p
+                  initial={{ opacity: 0, y: -100 }}
+                  transition={{ duration: 1, type: "spring", stiffness: 50 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  className='text-white w-[180px] text-center mt-6 font-bold'
+                >
+                  اشتراک
+                </motion.p>
               </Link>
             </div>
-            <div className="flex justify-center">
-              <Link href={'/Tafazol'}>
-                <img src='/homepage/icons/minusIcon.svg' alt="minus icon" height='150px' width='150px'/>
-                <p className="text-white w-[180px] flex justify-center absolute mt-6 font-bold">تفاضل</p>
-
+            <div className='flex justify-center'>
+              <Link href={"/Tafazol"}>
+                <motion.img
+                  variants={imgVariants}
+                  whileInView='visible'
+                  initial='hidden'
+                  src='/homepage/icons/minusIcon.svg'
+                  alt='minus icon'
+                  height='150px'
+                  width='150px'
+                />
+                <motion.p
+                  initial={{ opacity: 0, y: -100 }}
+                  transition={{ duration: 1, type: "spring", stiffness: 50 }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  className='text-white w-[180px] text-center mt-6 font-bold'
+                >
+                  تفاضل
+                </motion.p>
               </Link>
             </div>
           </div>
